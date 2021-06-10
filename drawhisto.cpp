e477bbb742afd3a12c02c0aa3851508b08bc039f
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2021-06-10 20:41:26 +0900

    [bugfix]

--- drawhisto.cpp
+++ drawhisto.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <TROOT.h>
+#include <TMath.h>
 #include <TFile.h>
 #include <TStyle.h>
 #include <TText.h>
@@ -16,12 +17,11 @@
 using namespace std;
 
 int main (int argc, char** argv){
-    TRint rootapp("app",&argc,argv);
 
     // Default input file parameters
-    int exp_date = 20210611;
-    int slot_no = 1;
-    int input_no = 1;
+    string exp_date = "20210611";
+    string slot_no = "1";
+    string input_no = "1";
 
     // Input file parameters passed by argument
     if (argc != 5){
@@ -29,13 +29,13 @@ int main (int argc, char** argv){
         cout << "enter 'd' to use the default value" << endl;
         exit(1);
     }
-    if (argv[1] != "d") exp_date = stoi(argv[1]);
-    int run_time = stoi(argv[2]);
-    if (argv[3] != "d") slot_no = stoi(argv[3]);
-    if (argv[4] != "d") input_no = stoi(argv[4]);
+    if (argv[1] != "d") exp_date = argv[1];
+    string run_time = argv[2];
+    if (argv[3] != "d") slot_no = argv[3];
+    if (argv[4] != "d") input_no = argv[4];
 
     // CSV file to read data from
-    const char* filename = Form("./-%08d-%06d-Slot%01d-In%01d.csv",exp_date,run_time,slot_no,input_no);
+    const char* filename = Form("./-%s-%s-Slot%s-In%s.csv",exp_date.c_str(),run_time.c_str(),slot_no.c_str(),input_no.c_str());
 
     // Read live-time of measurement (cell B37)
     double timedur = -9999.;
@@ -47,15 +47,13 @@ int main (int argc, char** argv){
         cout << "Analyzing data file " << filename << "..." << endl;
     }
     string line;
-    int row = 0;
-    int col = 0;
+    int k = 0;
     while ( getline(readcell,line) ){ // read loop for entire CSV file
-        ++row;
         istringstream linestream(line);
         string item;
         while ( getline(linestream,item,',') ){ // read loop for each line
-            ++col;
-            if (row==37 && col==2){
+            ++k; // item counter
+            if (k==75){
                 timedur = stod(item); // live-time measurement duration in seconds
                 cout << "Measurement live-time = " << timedur << " seconds." << endl;
                 break;
@@ -83,12 +81,15 @@ int main (int argc, char** argv){
     T.SetTextAlign(21);
 
     // Create TCanvas (split vertically in half)
+    TRint rootapp("app",&argc,argv);
     TCanvas *c_raw = new TCanvas("c_raw");
     c_raw->Divide(1,2);
 
     // Energy histogram
     c_raw->cd(1);
 
+    double ll = 0.;
+    double ul = 4096.;
     tree->Draw("rawchannel>>ehisto(4096,0.,4096.)");
     TH1F *ehtree = (TH1F*)gROOT->FindObject("ehisto");
     ehtree->SetTitle("");
@@ -107,11 +108,12 @@ int main (int argc, char** argv){
     // Time histogram
     c_raw->cd(2);
 
-    tree->Draw(Form("rawtime>>thtree(%d,0.,%3.0f)",timedur,double(timedur)*TMath::Power(10.,6)));
+    tree->Draw(Form("rawtime>>thtree(%d,0.,%3.0f)",TMath::Nint(timedur),double(TMath::Nint(timedur))*TMath::Power(10.,6)));
     TH1D *thtree = (TH1D*)gROOT->FindObject("thtree");
     thtree->SetTitle("");
     thtree->Draw();
-
+    
+    filename = Form("./-%s-%s-Slot%s-In%s.csv",exp_date.c_str(),run_time.c_str(),slot_no.c_str(),input_no.c_str()); // re-set
     T.DrawTextNDC(.5,.93,Form("Raw time histogram %s",filename));
     thtree->GetXaxis()->SetTitleSize(0.07);
     thtree->GetXaxis()->SetTitle("Time (#mus)");
