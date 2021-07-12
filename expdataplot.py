80b97e3f1b944edcdc3347aa773ced8d59b1fbd9
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2021-07-12 15:06:52 +0900

    [add] vacuum gauge only option for SFC

--- expdataplot.py
+++ expdataplot.py
@@ -14,33 +14,32 @@ try:
     infileSlow = './SlowDATA/' + filenameSlow + '.csv'
     dataSlow = pd.read_csv(infileSlow)
     timedur = -int(sys.argv[3])
+    sl = slice(timedur,None)
     mode = sys.argv[4]
     plt.ioff()
     plt.rcParams["font.size"] = 20
     while True:
-        # SFC plot
-        figSFC = plt.figure(figsize=(20,17))
-        axSC = figSFC.add_subplot(3,1,1)
-        axSV = figSFC.add_subplot(3,1,2)
-        axSP = figSFC.add_subplot(3,1,3)
-        axSC.set_xlabel('Time (s)')
-        axSV.set_xlabel('Time (s)')
-        axSP.set_xlabel('Time (s)')
-        axSC.set_ylabel('Current (nA)')
-        axSC.set_yscale('log')
-        axSV.set_ylabel('Voltage (V)')
-        axSP.set_ylabel('Pressure (Pa)')
-        axSP.set_yscale('log')
-        figSFC.tight_layout()
-        plt.grid(which='major',color='black',linestyle='-')
-        plt.grid(which='minor',color='black',linestyle='dotted')
-        
+        # SFC plot        
         # Get last-modified datetime
         p = pathlib.Path(infileSFC)
         last_modified = datetime.fromtimestamp(p.stat().st_mtime).strftime("%Y/%m/%d %H:%M:%S")
             
         if mode == "SFC":
-            sl = slice(timedur,None)
+            figSFC = plt.figure(figsize=(20,17))
+            axSC = figSFC.add_subplot(3,1,1)
+            axSV = figSFC.add_subplot(3,1,2)
+            axSP = figSFC.add_subplot(3,1,3)
+            axSC.set_xlabel('Time (s)')
+            axSV.set_xlabel('Time (s)')
+            axSP.set_xlabel('Time (s)')
+            axSC.set_ylabel('Current (nA)')
+            axSC.set_yscale('log')
+            axSV.set_ylabel('Voltage (V)')
+            axSP.set_ylabel('Pressure (Pa)')
+            axSP.set_yscale('log')
+            figSFC.tight_layout()
+            plt.grid(which='major',color='black',linestyle='-')
+            plt.grid(which='minor',color='black',linestyle='dotted')
             axSC.set_title('SFCDATA Log (Last Update: %s)'%(last_modified))
             axSC.plot(data['Time (s)'][sl], data['Current'][sl],'-',label='Current')
             axSC.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
@@ -62,13 +61,38 @@ try:
             axSP.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
             plt.yscale('log')
         elif mode == "PBT":
-            sl = slice(timedur,None)
+            figSFC = plt.figure(figsize=(20,17))
+            axSC = figSFC.add_subplot(3,1,1)
+            axSV = figSFC.add_subplot(3,1,2)
+            axSP = figSFC.add_subplot(3,1,3)
+            axSC.set_xlabel('Time (s)')
+            axSV.set_xlabel('Time (s)')
+            axSP.set_xlabel('Time (s)')
+            axSC.set_ylabel('Current (nA)')
+            axSC.set_yscale('log')
+            axSV.set_ylabel('Voltage (V)')
+            axSP.set_ylabel('Pressure (Pa)')
+            axSP.set_yscale('log')
+            figSFC.tight_layout()
+            plt.grid(which='major',color='black',linestyle='-')
+            plt.grid(which='minor',color='black',linestyle='dotted')
             axSC.set_title('Primary Beam Tuning Log (Last Update: %s)'%(last_modified))
             axSC.plot(data['Time (s)'][sl], data['Current'][sl],'-',label='Current')
             axSC.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
             axSP.plot(data['Time (s)'][sl], data['CH1'][sl],'-',label='CH1')
             axSP.plot(data['Time (s)'][sl], data['CH2'][sl],'-',label='CH2')
             axSP.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
+            plt.yscale('log')
+        elif mode == "VAC":
+            figSFC = plt.figure(figsize=(17,10))
+            plt.title('SFCDATA Vacuum Gauge (Last Update: %s)'%(last_modified))
+            plt.plot(data['Time (s)'][sl], data['C1 - CH 1 (Pa)'][sl],'-',label='CH 1')
+            plt.plot(data['Time (s)'][sl], data['C1 - CH 2 (Pa)'][sl],'-',label='CH 2')
+            plt.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
+            plt.xlabel('Time (s)')
+            plt.ylabel('Pressure (Pa)')
+            plt.grid(which='major',color='black',linestyle='-')
+            plt.grid(which='minor',color='black',linestyle='dotted')
             plt.yscale('log')
 
         # Draw and save figure
@@ -87,7 +111,6 @@ try:
         # Plot
         figSlow = plt.figure(figsize=(17,10))
         plt.title('SlowDATA Vacuum Gauge (Last Update: %s)'%(last_modified))
-        sl = slice(timedur,None)
         plt.plot(data['Time (s)'][sl], data['C1 - CH 1 (Pa)'][sl],'-',label='CH 1')
         plt.plot(data['Time (s)'][sl], data['C1 - CH 2 (Pa)'][sl],'-',label='CH 2')
         plt.legend(bbox_to_anchor=(1.02,1),loc='upper left',borderaxespad=0,fontsize=14)
@@ -106,9 +129,9 @@ try:
         plt.close(figSlow)
 
         # Wait for 5 seconds
-        time.sleep(5)
         print("\r",end='')
         print('Plotting data; LAST UPDATE: %s (Type Ctrl+C to finish...)'%(datetime.now()),end='',flush=True)
+        time.sleep(5)
 
 except KeyboardInterrupt:
     print('\nFINISHED PLOT UPDATE')
